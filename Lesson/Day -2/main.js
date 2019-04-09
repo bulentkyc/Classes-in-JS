c9e3347a5b5b44466e6b623a5628c0176d7cad4b
bulentkyc <bulent.kayici@digitalcareerinstitute.org>
2019-04-09 15:47:07 +0200

    Tasks for second day completed

--- Lesson/Day -2/main.js
+++ Lesson/Day -2/main.js
@@ -60,3 +60,154 @@ console.log(articles);
 
 let summary = Article.createTodays();
 console.log(summary);
+
+// Static Properties
+class StaticProperty {
+    static myVariable = 'Bülent Kayici';
+}
+
+// we can use static properties just via class not instance
+console.log(StaticProperty.myVariable);
+
+// Statics and inheritance
+/* Animal class has static compare method and if some classes en=xtend this class,
+    they will clone this static method also. Afterwards if some one would like to
+    reach clone static method, they should call directly new class and then clone 
+    static method.
+
+    For example, Rabbit class extends Animal class and if you'd like to use compare static method
+    you have to write like this:
+
+    'Rabbit.compare'
+*/
+class Animal {
+    constructor(name, speed){
+        this.name = name;
+        this.speed = speed;
+    }
+
+    // If we won't send speed value to this method. It'll consider speed as 0;
+    run(speed = 0){
+        this.speed += speed;
+        console.log(`${this.name} runs with speed ${this.speed}.`);
+    }
+
+    static compare(animalA, animalB){
+        return animalA.speed - animalB.speed;
+    }
+
+}
+
+// This class will have own compare static method on the end
+class Rabbit extends Animal {
+    hide() {
+        console.log(`${this.name} hides!`);
+    }
+}
+
+let rabbits = [
+    new Rabbit('White Rabbit', 10),
+    new Rabbit('Black Rabbit', 5)
+];
+
+//!!!!!!!! We are calling Rabbit class not Animal class
+rabbits.sort(Rabbit.compare);
+
+rabbits[0].run();
+rabbits[1].run();
+console.log(`${rabbits[1].name} is winner!`);
+
+
+// Chapter - 4: Private and Protected properties and methods
+/*
+Public: () accessible from anywhere. They comprise the external interface. 
+Till now we were only using public properties and methods.
+
+Private: (#) accessible only from inside the class. These are for the internal interface.
+
+Protected: (_) accessible only from inside the class and those extending it.
+*/
+
+// Let's create a simple coffe machine :)
+class CoffeeMachine {
+    
+    //waterAmount = 0; // This may not work on some js compilers
+
+    constructor(power) {
+        this._waterAmount = 0;
+        this._power = power; // if it will be public you need to use setter also
+        console.log(`Coffee machine is created, power: ${power}`);
+    }
+
+    //
+    set waterAmount(value) {
+        if (value < 0 || value>300) {
+            throw new Error('Water amount is not appripriate')
+        }
+        this._waterAmount = value;
+    }
+
+    get waterAmount(){
+        return this._waterAmount;
+    }
+
+    /* If you use get without set this property will be define while taking instance.
+    After that you can not change it!
+    */
+    get power() {
+        return this._power;
+    }
+
+}
+
+// create the machine
+let cMachine = new CoffeeMachine(220);
+
+// add water
+//cMachine.waterAmount = 400; // Error more than 300
+//cMachine.waterAmount = -4; // Error less than 0
+cMachine.waterAmount = 40; // :)
+
+cMachine.power = 500; // Error or does not work
+
+cMachine._power = 500; // works, because
+/*
+Protected fields are naturally inheritable. Unlike private ones that we’ll see below.
+This part maybe not work on your computer.
+
+
+console.log(cMachine);
+
+// Private (#) - Not totally superted by Js machines. For now partially supported.
+
+class CoffeMachine{
+    
+    checkWater(water){
+        if (value < 0 || value > 300){
+            throw new Error('Water amount is not appripriate');
+        }
+    }
+
+    constructor(){
+        this.#waterLimit = 200;
+        this._waterAmount = 0;
+    }
+
+    set waterAmount(value){
+        this.#this.checkWater(value);
+        this._waterAmount = value;
+    }
+
+    get waterAmount () {
+        return this.waterAmount;
+    }
+
+}
+
+let coffeMachine = new CoffeMachine;
+
+coffeMachine.#checkWater();
+coffeMachine.#waterLimit = 1000;
+
+coffeMachine.waterAmount = 100;
+*/
---
+++ Task -2/main.js
@@ -0,0 +1,69 @@
+/* 
+Task - 1 : Create a class and name it Auto.
+
+Task - 2 : Request these parameters for Auto: brand, year, color.
+
+Task - 3 : Create a method inside Auto and name it logMyCar.
+
+Task - 4 : Create another method inside Auto and name it oilLevel.
+
+Task - 5 : Convert oilLevel to static.
+
+Task - 6 : Write a code inside logMyCar to log 'this'.
+
+Task - 7 : Create a variable inside constructor and name it 'oil'.
+    As default set oil 100;
+
+Task - 8 : Write a code inside 'oilLevel' to log on console this tatic text:
+    "use 'oilControl' method to see oil level".
+
+Task - 9 : Create a method and name it 'putOil'. It should take 
+    parameter. Pasrameter name is also 'oil'.
+
+Task - 10 : Write a code inside putOil to increase oil amount on Class.
+
+Task - 11 : Create a method to decrease oil on class and name it useOil. This method
+    will take also 'oil' named parameter.
+
+Task - 12 : Create a method oilControl to log oil to console.
+
+Task - 13 : Create an instance from Auto wit required parameters.
+
+Task - 14 : Call all methods!
+
+Task - 15 : Create a static variable to store country and name it country.
+
+Task - 16 : Create 'speed' property on constructor. As default value use 0.
+
+Task - 17: Create a method to set speed. This method can get - or + values as paramater.
+    Parameter name will be 'speed'.
+
+Task - 18 : Use getters and setters to prevent - values for spped on class.
+    If value is minus, log to console: 'speed can be just 0 or more'. Also log - value.
+    Then set speed 0. But if speed is positive then directly use same value.
+
+Task - 19 : Create a static method to compare two speed and name it 'race'. 
+    We will use this method with JS sort.
+
+Task - 20 : Create an array on outside of class and name it 'cars'. 
+    Inside array create some cars as you wish.
+
+Task - 21 : Set some differen speeds for your cars inside array.
+
+Task - 22 : Use JS sort with 'race' static method to race your cars. Afterwards
+    log winner on console.
+
+Task - 23 : Request max speed also for Auto. But this valuse just can be setted
+    on taking instance. You have to prevent changings via instance.
+
+    Hint: You have to use protected variables and getter/setters
+
+
+
+
+
+
+
+
+
+*/
